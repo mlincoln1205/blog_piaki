580d964d6ca75888a6c4e4581bad8df334d06133
Lincoln Marcolongo <mlincoln1205@gmail.com>
2021-02-23 18:55:06 +0100

    Added Description to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
     has_many_attached :photos
     has_rich_text :rich_body
+    validates :description, length: { maximum: 360,
+        too_long: "%{count} characters is the maximum allowed" }
 end
