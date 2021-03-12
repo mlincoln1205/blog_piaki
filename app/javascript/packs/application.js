276d5cc10acf3101eb4d296316817df246d5f561
Lincoln Marcolongo <mlincoln1205@gmail.com>
2021-03-12 10:45:36 +0100

    Commit update before devise

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("trix")
+require("@rails/actiontext")
 
 import 'bootstrap';
 
@@ -14,5 +16,3 @@ document.addEventListener('turbolinks:load', () => {
     loadDynamicBannerText();
     weatherInfosHome();
   });
-require("trix")
-require("@rails/actiontext")
