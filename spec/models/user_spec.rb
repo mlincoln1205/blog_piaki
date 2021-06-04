682ce48abe4f472feaaaa31178eebb83b124463d
Lincoln Marcolongo <mlincoln1205@gmail.com>
2021-06-04 22:53:25 +0200

    Tests models article and user ok

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'Validate User' do
+    it 'is valid?' do
+      user = User.new
+      user.first_name = 'John'
+      user.last_name = 'Rambo'
+      user.email = 'john.rambo@john.com'
+      user.password = 'password'
+      expect(user.valid?).to be true
+    end
+    it 'is not valid?' do
+      user = User.new
+      user.first_name = 'John'
+      user.last_name = 'Rambo'
+      user.password = 'password'
+      expect(user.valid?).to be false
+    end
+  end
+end
